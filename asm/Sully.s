2ba9934eac5503b7fb104de4110f75f8e15e3ac4
terngkub <terngnattapol@gmail.com>
2019-01-25 21:13:24 +0100

    Sully.s can now create file

--- asm/Sully.s
+++ asm/Sully.s
@@ -1,5 +1,6 @@
 extern _asprintf
 extern _printf
+extern _dprintf
 extern _system
 extern _free
 
@@ -7,13 +8,14 @@ section .text
 global _main
 
 _main:
+	; init
 	push	rbp
 	mov		rbp, rsp
 	sub		rsp, 160
 
 	; check integer
-	; cmp		rax, 0
-	; jle		exit
+	cmp		rax, 0
+	jle		exit
 
 	; check Sully.s
 	;dec 	rax
@@ -24,32 +26,33 @@ _main:
 	mov		rdx, [rel i]
 	call	_asprintf
 
+	; debug - print filename
 	mov		rdi, [rsp]
 	call	_printf
 
 	; create file
-	; lea		rdi, [rel rsp]
-	; mov		rsi, 0x202
-	; mov		rdx, 0o644
-	; mov		rax, 0x2000005
-	; syscall
-	; cmp		rax, -1
-	; je		exit
-	; mov		[rsp+4], rax
+	mov		rdi, [rsp]
+	mov		rsi, 0x202
+	mov		rdx, 0o644
+	mov		rax, 0x2000005
+	syscall
+	cmp		rax, -1
+	je		exit
+	mov		[rsp+4], rax
 
-	; ; write file
-	; mov		rdi, [rsp+4]
-	; lea		rsi, [rel format]
-	; lea		rdx, [rel format]
+	; write file
+	mov		rdi, [rsp+4]
+	lea		rsi, [rel format]
+	;lea		rdx, [rel format]
 	; mov		rcx, 10
 	; mov		r8, 9
 	; mov		r9, 34
-	; call	_dprintf
+	call	_dprintf
 
-	; ; close file
-	; mov		rdi, [rsp+4]
-	; mov		rax, 0x2000006
-	; syscall
+	; close file
+	mov		rdi, [rsp+4]
+	mov		rax, 0x2000006
+	syscall
 
 	; ; compile
 
@@ -63,4 +66,7 @@ exit:
 section .data
 
 i: dd 5
-filename: db "Sully_%d.c", 0
+filename: db "Sully_%d.s", 0
+format: db "abc", 0
+compile: db "clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d", 0
+run: db "./Sully_%d", 0
